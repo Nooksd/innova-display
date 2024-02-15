c9729dac93cb1da00857e2d1e4afc2d8f3de117c
Nooksd <69806396+Nooksd@users.noreply.github.com>
2024-02-15 11:18:32 -0300

    Add files via upload

--- display/frontend/src/services/Http.js
+++ display/frontend/src/services/Http.js
@@ -11,8 +11,8 @@ export const solarzProxyApi = axios.create({
     baseURL: 'https://192.168.1.102:3000/',
     headers: {
         'Content-Type': 'aplication/json',
-        'user': import.meta.env.VITE_USER,
-        'password': import.meta.env.VITE_PASSWORD
+        'user': 'suportetecnico@innova-energy.com.br',
+        'password': 'senha123'
     }
 });
 
