3fa4d6a181b6c0a6b35060d5c6af4932e542340d
Nooksd <69806396+Nooksd@users.noreply.github.com>
2024-02-15 15:02:47 -0300

    Add files via upload

--- frontend/src/services/Http.js
+++ frontend/src/services/Http.js
@@ -8,7 +8,7 @@ export const whetherApi = axios.create({
 });
 
 export const solarzProxyApi = axios.create({
-    baseURL: 'https://192.168.1.102:3000/',
+    baseURL: 'https://proxy-innova.onrender.com/',
     headers: {
         'Content-Type': 'aplication/json',
         'user': 'suportetecnico@innova-energy.com.br',
