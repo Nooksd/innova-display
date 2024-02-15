1767529297e144203f7afa42f730e23451d124be
Nooksd <69806396+Nooksd@users.noreply.github.com>
2024-02-15 13:50:36 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -30,23 +30,108 @@ const solarzApi = axios.create({
 
 app.get('/:id', basicAuth, async (req, res) => {
   try {
+
+    // Faz a solicitação para a API
     const response = await solarzApi.get(`/seller/plant/power?id=${req.params.id}`);
+
+    // Retorna a resposta da API para o cliente
     res.send(response.data);
   } catch (error) {
+
+    // Se houver um erro, envia uma resposta de erro para o cliente
     res.status(500).send('Erro ao acessar a API');
   }
 });
 
 app.get('/range/:id', basicAuth, async (req, res) => {
-  // O mesmo que o seu código atual...
+  const currentDate = new Date();
+
+  const sixteenDaysAgo = new Date(currentDate.getTime() - (16 * 24 * 60 * 60 * 1000));
+  const oneDayAgo = new Date(currentDate.getTime() - (1 * 24 * 60 * 60 * 1000));
+
+  const yearP = sixteenDaysAgo.getFullYear();
+  const monthP = (sixteenDaysAgo.getMonth() < 9 ? '0' : '') + (sixteenDaysAgo.getMonth() + 1);
+  const dayP = (sixteenDaysAgo.getDate() < 10 ? '0' : '') + sixteenDaysAgo.getDate();
+
+  const year = oneDayAgo.getFullYear();
+  const month = (oneDayAgo.getMonth() < 9 ? '0' : '') + (oneDayAgo.getMonth() + 1);
+  const day = (oneDayAgo.getDate() < 10 ? '0' : '') + oneDayAgo.getDate();
+
+  const dataPassada = yearP + "-" + monthP + "-" + dayP;
+  const dataAtual = year + "-" + month + "-" + day;
+
+  const rangeData = {
+    "fromLocalDate": dataPassada,
+    "plantId": req.params.id,
+    "toLocalDate": dataAtual
+  }
+
+  try {
+
+    // Faz a solicitação para a API
+    const response = await solarzApi.post('/seller/plant/energy/dayRange', rangeData, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    )
+
+    // Retorna a resposta da API para o cliente
+    res.send(response.data);
+  } catch (error) {
+
+    // Se houver um erro, envia uma resposta de erro para o cliente
+    res.status(500).send(error)
+  }
 });
 
 app.get('/powerCurve/:id', basicAuth, async (req, res) => {
-  // O mesmo que o seu código atual...
+
+  const currentDate = new Date();
+  
+  const year = currentDate.getFullYear();
+  const month = (currentDate.getMonth() < 9 ? '0' : '') + (currentDate.getMonth() + 1);
+  const day = (currentDate.getDate() < 10 ? '0' : '') + currentDate.getDate();
+
+  const date = year + "-" + month + "-" + day;
+
+
+  try {
+
+    // Faz a solicitação para a API
+    const response = await solarzApi.get(`/seller/plantRealTimeData/powerCurve/day/${date}/unitePortals/true/plantId/${req.params.id}`)
+
+    // Retorna a resposta da API para o cliente
+    res.send(response.data);
+
+  } catch (error) {
+
+    // Se houver um erro, envia uma resposta de erro para o cliente
+    res.status(500).send(error)
+  }
 });
 
+
 app.get('/comparison/:id1/:id2/:id3', basicAuth, async (req, res) => {
-  // O mesmo que o seu código atual...
+  try {
+
+    // Cria variáveis para função
+    const allTotals = [];
+    const ids = [req.params.id1, req.params.id2, req.params.id3];
+
+    // pega todos os dados na api com os 3 ids
+    for (const id of ids) {
+      const response = await solarzApi.get(`/seller/plant/power?id=${id}`);
+      allTotals.push(response.data.totalGenerated);
+    }
+
+    // Retorna a resposta da API para o cliente
+    res.send(allTotals);
+  } catch (error) {
+
+    // Se houver um erro, envia uma resposta de erro para o cliente
+    res.status(500).send('Erro ao acessar a API');
+  }
 });
 
 app.listen(PORT, () => {
